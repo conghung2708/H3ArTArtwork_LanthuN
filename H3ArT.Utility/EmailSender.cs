830f56e145fc97efd3b947e5f08c1b3dcde93f04
Minh Tai <minhtai7112003@gmail.com>
2024-03-07 02:55:13 +0700

    send_mail

--- H3ArT.Utility/EmailSender.cs
+++ H3ArT.Utility/EmailSender.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,30 @@ namespace H3ArT.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask; /////
+            string _from = "nguyenferbvn@gmail.com";
+            MailMessage message = new MailMessage(_from, email, subject, htmlMessage);
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            message.IsBodyHtml = true;
+
+            message.ReplyToList.Add(new MailAddress(_from));
+            message.Sender = new MailAddress(_from);
+
+            using var smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential("nguyenferbvn@gmail.com", "xhpy chdu vevr emlk");
+
+            try
+            {
+                await smtpClient.SendMailAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
