cc478c941abade8a45ff246cdbea3174ee102059
MamMam03 <155883794+MamMam03@users.noreply.github.com>
2024-04-04 20:39:17 +0700

    Done AdminDashboard

---
+++ Source_Code/H3ArTArtwork_LanthuN/H3ArT.Models/ViewModels/DashboardVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3ArT.Models.ViewModels
+{
+    public class DashboardVM
+    {
+        public int NumberOfUsers { get; set; }
+        public int NumberOfArtworks { get; set; }
+        public int NumberOfOrders { get; set; }
+        public double NumberOfSales { get; set; }
+
+    }
+}
